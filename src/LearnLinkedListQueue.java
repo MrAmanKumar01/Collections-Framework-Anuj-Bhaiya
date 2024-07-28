f63ea2c9fc90d3d773794ef9667fab0b609ca5d7
Aman Kumar <143094665+MrAmanKumar01@users.noreply.github.com>
2024-07-28 11:04:23 +0530

    Update LearnLinkedListQueue.java

--- src/LearnLinkedListQueue.java
+++ src/LearnLinkedListQueue.java
@@ -7,17 +7,17 @@ public class LearnLinkedListQueue {
 
         Queue<Integer> queue = new LinkedList<>();
 
-        queue.offer(12);
+        queue.offer(12);  //adjacent method add() --> can throw an exception upon false whereas offer() doesn't
         queue.offer(24);
         queue.offer(36);
 
         System.out.println(queue);
 
-        System.out.println(queue.poll());
+        System.out.println(queue.poll());  //adjacent method remove()
 
         System.out.println(queue);
 
-        System.out.println(queue.peek());
+        System.out.println(queue.peek());  //adjacent method element()
 
     }
 }
