00da4c37871628e79cbfa5b8f7c8e92b2491e40b
Aman Kumar <143094665+MrAmanKumar01@users.noreply.github.com>
2024-07-28 10:59:09 +0530

    Update LearnArrayList.java
    LinkedList is used to implement List as well as Queue interface.

--- src/LearnArrayList.java
+++ src/LearnArrayList.java
@@ -7,7 +7,8 @@ public class LearnArrayList {
 
     public static void main(String[] args) {
 
-        List<Integer> list = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        // List<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
